4d01a50fefff8cb761423c5f9d4ac521311fcb7e
Jean Carlos <jean.cristovao@sankhya.com.br>
2026-03-11 19:58:32 -0300

    fix: corrigido sequelizerc para apontar para o dist

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import sequelize from "./database/database.js";
 
 const app = express();
 const port = ENV.PORT;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+async function main() {
+  await sequelize.authenticate();
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+main();
